8d77ea9011a73ca64d2bc708070a2f2fbc672dcc
shrikar007 <shrikar.vaitala@perennialsys.com>
2019-03-26 13:02:43 +0530

    use of select in go languaage

--- concurrence/src/github.com/shrikar007/selectgo/main.go
+++ concurrence/src/github.com/shrikar007/selectgo/main.go
@@ -1,1 +1,34 @@
-package selectgo
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func server1(ch chan string) {
+//time.Sleep(5 * time.Second)
+ch <- "from server1"
+}
+func server2(ch chan string) {
+//time.Sleep(3* time.Second)
+ch <- "from server2"
+
+}
+func main() {
+//output1 := make(chan string)
+//output2 := make(chan string)
+
+ch:=make(chan string,2)
+go server2(ch)
+go server1(ch)
+time.Sleep(1*time.Second)
+select {
+case s1 := <-ch:
+fmt.Println(s1)
+case s2 := <-ch:
+fmt.Println(s2)
+
+}
+
+fmt.Println("The End")
+}
