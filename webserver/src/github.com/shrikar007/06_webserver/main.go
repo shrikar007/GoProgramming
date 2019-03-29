7b1733440390d6c27edd98e094c1797b648146af
shrikar007 <shrikar.vaitala@perennialsys.com>
2019-03-29 11:05:52 +0530

    check the valid password in html and go

--- webserver/src/github.com/shrikar007/06_webserver/main.go
+++ webserver/src/github.com/shrikar007/06_webserver/main.go
@@ -10,31 +10,31 @@ type Login struct {
 	username  string
 	password string
 }
+var pass= "shri"
 func main(){
-	t1, err := template.ParseFiles("login.html")
-	//t2, err1 := template.ParseFiles("display1.html")
-	if err != nil{
-		log.Fatalln(err)
-	}
-	h1:=func (w http.ResponseWriter,r *http.Request){
-
-		var l Login
-		l.username = r.FormValue("user")
-		l.password = r.FormValue("pass")
-		t1.Execute(w,l)
-		if l.password =="shri"{
-			fmt.Println("Login successfull")
-		} else {
-			fmt.Println("Login error")
-		}
-		//fmt.Println( l.username)
-		//fmt.Println( l.password)
-		if err != nil{
-			log.Fatal(err)
-		}
-		//t2.Execute(w,l)
-	}
-	http.HandleFunc("/login",h1)
-	fmt.Println(http.ListenAndServe(":5004",nil))
+	http.HandleFunc("/login1",h1)
+	fmt.Println(http.ListenAndServe(":4505",nil))
 }
+func h1 (w http.ResponseWriter,r *http.Request) {
+	var l Login
+	var Success bool
+	t1, err := template.ParseFiles("login.html")
 
+	if err != nil {
+		log.Fatal(err)
+	}
+	l.username = r.FormValue("user")
+	l.password = r.FormValue("pass")
+	if r.FormValue("pass") == pass {
+		Success=true
+	} else  {
+		Success=false
+	}
+	err1 := t1.ExecuteTemplate(w,"login.html",Success)
+	if err1!=nil{
+		log.Fatal(err1)
+	}
+
+
+
+}
---
+++ webserver/src/github.com/shrikar007/06_webserver/work.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+
+	var s1="shri"
+	var s2= "shri"
+
+	if s1==s2{
+
+		fmt.Println("Same")
+	}else{
+		fmt.Println("not same")
+	}
+
+
+}
